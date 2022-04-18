7fa8d13de66939147191025839a1a3eab19db34a
romycavallaro <romycavallaro@gmail.com>
2022-04-18 11:24:24 -0300

    Agregando admin

--- AppFamilia/admin.py
+++ AppFamilia/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import * 
 
 # Register your models here.
+
+admin.site.register(Padres)
+admin.site.register(Hermanos)
+admin.site.register(Esposo)
+admin.site.register(Hijos)
